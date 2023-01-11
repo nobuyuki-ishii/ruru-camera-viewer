b3a1fb55d16cb509f299042e9a8c706f634ebfb6
nobuyuki-ishii <nby2ki@gmail.com>
2023-01-12 07:14:23 +0900

    インデント修正

--- src/App.svelte
+++ src/App.svelte
@@ -3,29 +3,29 @@
 	import type { Unsubscriber } from 'svelte/store';
 	import Date from './Date.svelte';
 	import Calendar from './Calendar.svelte';
-    import { ym, pics } from './stores';
+	import { ym, pics } from './stores';
 	import 'luminous-lightbox/dist/luminous-basic.min.css';
 
 	const title: string = "ルルカメラ";
 	const api_url: string = "//141-lab.com/ruru_api/?ym=";
 
-  	// dateの更新で呼ばれる
+	// dateの更新で呼ばれる
 	const unscribeYm: Unsubscriber = ym.subscribe(value => {
-        let ymParam: string = $ym.replace('-', '');
+		let ymParam: string = $ym.replace('-', '');
 
-        // 写真リストを取得
-        fetch(api_url + ymParam)
-        .then((response) => {
-            return response.json();
-        })
-        .then((data) => {
+		// 写真リストを取得
+		fetch(api_url + ymParam)
+		.then((response) => {
+			return response.json();
+		})
+		.then((data) => {
 			console.log(data);
-            pics.set(data);
-        })
-        .catch(error => {
-            console.log("api error.");
-        });
-    });
+			pics.set(data);
+		})
+		.catch(error => {
+			console.log("api error.");
+		});
+	});
 	onDestroy(unscribeYm);
 </script>
 
