c91d6ddcd6091f94cae9cc1dd8843ea30a16b9b3
nobuyuki-ishii <nby2ki@gmail.com>
2023-01-12 09:04:54 +0900

    インデント修正

--- src/Date.svelte
+++ src/Date.svelte
@@ -1,35 +1,35 @@
 <script lang="ts">
-    import { ym } from './stores';
+	import { ym } from './stores';
 
-    function next(): void {
-        // 1ヶ月後
-        ymCalc(1);
-    }
+	function next(): void {
+		// 1ヶ月後
+		ymCalc(1);
+	}
 
-    function prev(): void {
-        // 1ヶ月前
-        ymCalc(-1);
-    }
+	function prev(): void {
+		// 1ヶ月前
+		ymCalc(-1);
+	}
 
-    // yyyy-mmにxヶ月を加算
-    function ymCalc(addM: number): void {
-        let ymWk: string[] = $ym.split('-');
-        let y: number = Number(ymWk[0]);
-        let m: number = Number(ymWk[1]) + addM;
+	// yyyy-mmにxヶ月を加算
+	function ymCalc(addM: number): void {
+		let ymWk: string[] = $ym.split('-');
+		let y: number = Number(ymWk[0]);
+		let m: number = Number(ymWk[1]) + addM;
 
-        if (m == 13) {
-            // 年またぎ
-            y++;
-            m = 1;
-        } else if (m == 0) {
-            // 年またぎ
-            y--;
-            m = 12;
-        }
+		if (m == 13) {
+			// 年またぎ
+			y++;
+			m = 1;
+		} else if (m == 0) {
+			// 年またぎ
+			y--;
+			m = 12;
+		}
 
-        // yyyy-mm形式にする
-        ym.set(String(y) + '-' + ('0' + String(m)).slice(-2));
-    }
+		// yyyy-mm形式にする
+		ym.set(String(y) + '-' + ('0' + String(m)).slice(-2));
+	}
 </script>
 
 <input type="button" class="btn btn-light" value="&lt;" on:click={prev}>
