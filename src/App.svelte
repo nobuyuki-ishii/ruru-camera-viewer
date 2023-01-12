515aeff270a6f5d793f6ef78820ab1da2f1ada7e
nobuyuki-ishii <nby2ki@gmail.com>
2023-01-12 19:51:19 +0900

    インデント修正

--- src/App.svelte
+++ src/App.svelte
@@ -1,40 +1,40 @@
 <script lang="ts">
-	import { onDestroy } from 'svelte';
-	import type { Unsubscriber } from 'svelte/store';
-	import Date from './Date.svelte';
-	import Calendar from './Calendar.svelte';
-	import { ym, pics } from './stores';
-	import 'luminous-lightbox/dist/luminous-basic.min.css';
+  import { onDestroy } from 'svelte';
+  import type { Unsubscriber } from 'svelte/store';
+  import Date from './Date.svelte';
+  import Calendar from './Calendar.svelte';
+  import { ym, pics } from './stores';
+  import 'luminous-lightbox/dist/luminous-basic.min.css';
 
-	const title: string = "ルルカメラ";
-	const api_url: string = "//141-lab.com/ruru_api/?ym=";
+  const title: string = "ルルカメラ";
+  const api_url: string = "//141-lab.com/ruru_api/?ym=";
 
-	// dateの更新で呼ばれる
-	const unscribeYm: Unsubscriber = ym.subscribe(value => {
-		let ymParam: string = $ym.replace('-', '');
+  // dateの更新で呼ばれる
+  const unscribeYm: Unsubscriber = ym.subscribe(value => {
+    let ymParam: string = $ym.replace('-', '');
 
-		// 写真リストを取得
-		fetch(api_url + ymParam)
-		.then((response) => {
-			return response.json();
-		})
-		.then((data) => {
-			console.log(data);
-			pics.set(data);
-		})
-		.catch(error => {
-			console.log("api error.");
-		});
-	});
-	onDestroy(unscribeYm);
+    // 写真リストを取得
+    fetch(api_url + ymParam)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      pics.set(data);
+    })
+    .catch(error => {
+      console.log("api error.");
+    });
+  });
+  onDestroy(unscribeYm);
 </script>
 
 <main>
-	<div id="header" class="sticky-top bg-white mb-3">
-		<h1>{title}</h1>
-		<Date/>
-	</div>
-	<Calendar/>
+  <div id="header" class="sticky-top bg-white mb-3">
+    <h1>{title}</h1>
+    <Date/>
+  </div>
+  <Calendar/>
 </main>
 
 <style>
